439e555bcb3bcf85a09b13a9b806f3bd129c5f52
kbermude <42000003+kbermude@users.noreply.github.com>
2018-12-11 13:29:44 -0500

    Update AsusRoge.java

--- ManejoComputadoras/src/emsamablajecomputadoras/creacional/AsusRoge.java
+++ ManejoComputadoras/src/emsamablajecomputadoras/creacional/AsusRoge.java
@@ -1,40 +1,48 @@
 package emsamablajecomputadoras.creacional;
 
 public class AsusRoge extends ControladorComputadora {
-
+	public AsusRoge() {
+		
+		computador = new Computador();
+		
+	}
 	@Override
 	public void asignarMarca() {
-		// TODO Auto-generated method stub
+		computador.setMarca("Asus");
 		
 	}
 
 	@Override
 	public void asignarrModelo() {
-		// TODO Auto-generated method stub
+		computador.setModelo("ROG");
+		
 		
 	}
 
 	@Override
 	public void Almacenamiento() {
-		// TODO Auto-generated method stub
+		computador.setTipoAlmacenamiento("HDD");
+		computador.setAlmacenamiento(1000);
 		
 	}
 
 	@Override
 	public void agregarRam() {
-		// TODO Auto-generated method stub
+		computador.setTipoRAM("DDR4");
+		computador.setRAM(32);
+		
 		
 	}
 
 	@Override
 	public void Mainboard() {
-		// TODO Auto-generated method stub
+		computador.setPlaca(new Mainboard("Strix", "x99"));
 		
 	}
 
 	@Override
 	public void asignarSistemaOperativo() {
-		// TODO Auto-generated method stub
+		computador.setOS(new SistemaOperativo("Windows", 64, "10 PRO"));
 		
 	}
 
